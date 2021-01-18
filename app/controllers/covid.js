1a1718c82fee0e732b0ebe5990472d55d95fb865
Ariel <ari.atar1997@gmail.com>
2021-01-18 15:01:25 -0300

    going on with read-csv

--- app/controllers/covid.js
+++ app/controllers/covid.js
@@ -1,1 +1,20 @@
-exports.readCsv = (req, res) => res.send('OK');
+const csv = require('csvtojson');
+const request = require('request');
+const logger = require('../logger');
+
+exports.readCsv = async (req, res) => {
+  let response = [];
+  await csv()
+    .fromStream(request.get('https://sisa.msal.gov.ar/datos/descargas/covid-19/files/Covid19Casos.csv'))
+    .subscribe(
+      json =>
+        new Promise(resolve => {
+          resolve();
+          logger.info(json);
+          response = [...response, json];
+        }),
+      null,
+      null
+    );
+  return res.send(response);
+};
