ae7b84ead54c4728f0bb1d6a8003222e407b4ba8
Ariel <ari.atar1997@gmail.com>
2021-01-31 12:16:22 -0300

    minor changes

---
+++ app/constants.js
@@ -0,0 +1,6 @@
+exports.DESCRIPTOR = {
+  name: 'covid-api',
+  version: '0.1.0'
+};
+
+exports.API_BASE_URL = '/api/v1';
--- app/routes.js
+++ app/routes.js
@@ -11,21 +11,18 @@ const {
   summary
 } = require('./controllers/covid');
 
-const descriptor = {
-  name: 'covid-api',
-  version: '0.1.0'
-};
+const { DESCRIPTOR, API_BASE_URL } = require('./constants');
 
 exports.init = app => {
-  app.get('/', (req, res) => res.send(descriptor));
-  app.get('/count', count);
-  app.get('/get-cases', getCases);
-  app.get('/last_update', lastUpdate);
-  app.get('/province/:slug', provinceCases);
-  app.get('/province/:slug/count', provinceCount);
-  app.get('/province/:slug/stats', provinceStats);
-  app.get('/province/:slug/summary', provinceSummary);
-  app.get('/provinces', provinces);
-  app.get('/stats', stats);
-  app.get('/summary', summary);
+  app.get('/', (req, res) => res.send(DESCRIPTOR));
+  app.get(`${API_BASE_URL}/count`, count);
+  app.get(`${API_BASE_URL}/get-cases`, getCases);
+  app.get(`${API_BASE_URL}/last_update`, lastUpdate);
+  app.get(`${API_BASE_URL}/province/:slug`, provinceCases);
+  app.get(`${API_BASE_URL}/province/:slug/count`, provinceCount);
+  app.get(`${API_BASE_URL}/province/:slug/stats`, provinceStats);
+  app.get(`${API_BASE_URL}/province/:slug/summary`, provinceSummary);
+  app.get(`${API_BASE_URL}/provinces`, provinces);
+  app.get(`${API_BASE_URL}/stats`, stats);
+  app.get(`${API_BASE_URL}/summary`, summary);
 };
