a961e3e6eb0446b75eb94efad03c14d7c456fa26
Agustín <27942554+agustin-golmar@users.noreply.github.com>
2021-03-12 16:04:36 -0300

    Add Swagger endpoint to descriptor.

--- app/constants.js
+++ app/constants.js
@@ -4,7 +4,8 @@ exports.DESCRIPTOR = {
   contributors: project.contributors,
   description: project.description,
   name: project.name.substring(1 + project.name.indexOf('/')),
-  version: project.version
+  version: project.version,
+  swagger: 'https://covid19api.it.itba.edu.ar/api/v0.1.0/swagger/'
 };
 
 exports.API_BASE_URL = `/api/v${project.version}`;
