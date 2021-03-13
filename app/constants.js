12f6f787b1ceb9fa08973eead9ed3baa804e4120
Agustín Golmar <agustin.golmar@gmail.com>
2021-03-12 23:03:06 -0300

    Release v0.1.0 artifact.

--- app/constants.js
+++ app/constants.js
@@ -5,7 +5,7 @@ exports.DESCRIPTOR = {
   description: project.description,
   name: project.name.substring(1 + project.name.indexOf('/')),
   version: project.version,
-  swagger: 'https://covid19api.it.itba.edu.ar/api/v0.1.0/swagger/'
+  swagger: `https://covid19api.it.itba.edu.ar/api/v${project.version}/swagger/`
 };
 
 exports.API_BASE_URL = `/api/v${project.version}`;
